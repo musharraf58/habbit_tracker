34cce50dbe25a108844c8dd72077ca0844a651e6
Musharraf Amadbekov <amadbekovmusharraf675@gmail.com>
2026-09-16 14:39:06 +0600

    Add user login with JWT

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from app.security import hash_password, verify_password, create_access_token
 from app.database import get_db
 from app.models import User
 from app.schemas import UserCreate
@@ -37,4 +37,30 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     return {
         "message": "User registered successfully",
         "user_id": new_user.id
+    }
+
+
+@router.post("/login")
+def login(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not verify_password(user.password, existing_user.password_hash):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        {"sub": str(existing_user.id)}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
     }
--- app/security.py
+++ app/security.py
@@ -1,4 +1,6 @@
 import bcrypt
+from datetime import datetime, timedelta, timezone
+from jose import jwt
 
 
 def hash_password(password: str) -> str:
@@ -12,3 +14,20 @@ def verify_password(password: str, hashed_password: str) -> bool:
     password_bytes = password.encode("utf-8")
     hashed_bytes = hashed_password.encode("utf-8")
     return bcrypt.checkpw(password_bytes, hashed_bytes)
+
+
+SECRET_KEY = "change-this-to-a-long-random-string"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
