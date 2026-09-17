dbb43ceb9fcec38ebf17d5208fb092cc33f9aab5
Musharraf Amadbekov <amadbekovmusharraf675@gmail.com>
2026-09-17 11:47:35 +0600

    Add habits database model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.database import Base
 
 
@@ -8,3 +10,15 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False, index=True)
     password_hash = Column(String, nullable=False)
+
+    habits = relationship("Habit", back_populates="owner")
+
+
+class Habit(Base):
+    __tablename__ = "habits"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="habits")
